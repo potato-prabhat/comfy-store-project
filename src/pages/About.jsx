d053da922f42bfc250669cb1e2f766676f568951
Prabhat <krsinghp8@gmail.com>
2024-06-17 06:51:48 -0400

    about page and navbar

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,26 @@
 const About = () => {
-  return <div>About</div>
+  return (
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+        <h1 className="text-4xl font-bold leading-none tracking-tight">
+          We love
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto text-justify">
+        I'm baby meditation yr quinoa adaptogen, viral deep v literally iceland
+        whatever. Direct trade prism listicle blue bottle helvetica. Post-ironic
+        ramps pickled neutra, live-edge pug kombucha tofu unicorn air plant.
+        Squid locavore VHS, neutra umami scenester godard banh mi portland blue
+        bottle marfa lo-fi jawn next level.
+      </p>
+    </>
+  )
 }
 export default About
